0520bd9359f5caadd3213ec6d42c89116a831e08
Domas <dom.r.creative@gmail.com>
2019-12-27 15:46:33 +0000

    New  portrait photos

--- assets/upload.bash
+++ assets/upload.bash
@@ -11,26 +11,25 @@ else
     echo "UPLOAD DIRECTORY is Empty!!!!!!!"
     exit
 fi
-
+ans=-1
 for filename in ./upload/*; do
 
     if [ ${filename:9:4} == "min-" ]
       then
         continue
     fi
-
-    while [[ "${ans}" > 5 ]] || [[ "${ans}" < 0 ]]; do
-    printf '\n'
-   # echo "MAKE SURE YOU HAVE THE MINIFIED FILE AS WELL ! ! ! !"
-    printf '\n'
-    echo "What category does ${filename:9} go to?"
-    echo "0 - People"
-    echo "1 - Compositions"
-    echo "2 - Food"
-    echo "3 - Landscapes"
-    echo "4 - Other"
-    echo "5 - SKIP THIS FILE"
-    read ans
+    
+    while (( "${ans}" > 5 )) || (( "${ans}" < 0 )); do
+      printf '\n'
+      printf '\n'
+      echo "What category does ${filename:9} go to?"
+      echo "0 - People"
+      echo "1 - Compositions"
+      echo "2 - Food"
+      echo "3 - Landscapes"
+      echo "4 - Other"
+      echo "5 - SKIP THIS FILE"
+      read ans
     done
 
     if [ "${ans}" == 5 ]; then
@@ -40,10 +39,10 @@ for filename in ./upload/*; do
 
     categories=(people compositions food landscapes other)
 
-    echo "Moving main file to the designated folder..."
+    echo "Moving $filename to pics/${categories[ans]}..."
     mv "$filename" pics/${categories[ans]}
     #mv "${filename:0:9}min-${filename:9}" pics/${categories[ans]}
-    echo "Adding filename to order.csv..."
+    echo "Adding filename (${filename:9}) to order.csv (pics/${categories[ans]}/order.csv)..."
     echo "${filename:9}" >> pics/${categories[ans]}/order.csv
     echo "Done! ( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)"
     printf '\n'
